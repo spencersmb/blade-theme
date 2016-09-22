418012a17bcdefc6292f8619426e2dd4406a62a6
spencersmb <spencer.bigum@gmail.com>
2016-09-22 19:43:32 -0400

    sept update

--- inc/tgm/plugin-activation.php
+++ inc/tgm/plugin-activation.php
@@ -66,7 +66,7 @@ function sprout_register_required_plugins() {
             'slug'                  => 'redux', // The plugin slug (typically the folder name)
             'source'                => get_template_directory() . '/inc/plugins/redux-framework.zip', // The plugin source
             'required'              => true, // If false, the plugin is only 'recommended' instead of required
-            'version'               => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
+            'version'               => '3.6.0', // E.g. 1.0.0. If set, the active plugin must be this version or higher, otherwise a notice is presented
             'force_activation'      => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch
             'force_deactivation'    => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins
             'external_url'          => '', // If set, overrides default API URL and points to an external URL
